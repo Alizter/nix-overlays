be5467b10527f32ae15b79a53230a658ea320b2e
UpdateBot <ulrikstrid@users.noreply.github.com>
2026-07-27 13:53:52 +0000

    Update sources

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=eeac4f06ba6d4c5540c4838d13b31a2cbe0e104b";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=aac3a899cc6ce8d928dfc5818dafc093b7936010";
   };
 
   outputs =
